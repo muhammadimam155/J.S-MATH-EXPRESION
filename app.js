89d1d6e376cb0209411027e982e546bcc9914393
muhammadimam155 <imamaliatari@gmail.com>
2023-10-08 11:42:57 +0500

    math expertion

---
+++ app.js
@@ -0,0 +1,71 @@
+document.write("<h1> result </h1> <br>")
+var a = (10)
+document.write("the value of a is "+a+"<br><br><br><br>")
+var a =(++a)
+document.write("the  vlaue of ++a is "+a+"<br>")
+document.write("now the value of a is "+a+" <br><br><br><br>")
+var a =(a++)
+document.write("the  vlaue of a++ is "+a+"<br>")
+a++
+document.write("now the value of a is "+a+" <br><br><br><br>")
+var a =(--a)
+document.write("the  vlaue of --a is "+a+"<br>")
+document.write("now the value of a is "+a+" <br><br><br><br>")
+var a =(a--)
+document.write("the  vlaue of a-- is "+a+"<br>")
+a--
+document.write("now the value of a is "+a+" <br><br><br><br>")
+document.write("<h2>the end of Q.1</h2> <br><br>")
+
+
+
+
+
+
+document.write("<h2>the start of Q.2</h2> <br>")
+var a = 2
+var b = 1
+var result = --a - --b + ++b + b-- 
+
+document.write(" the value of a is "+a+"<br> ")
+document.write(" the value of b is "+b+"<br>")
+document.write(" the question is --a - --b + ++b + b-- <br>")
+document.write(" the answer is "+ result+" <br>")
+document.write("<h2>the end of Q.2</h2> <br><br>")
+
+
+
+
+// the start of Q.3
+var yourName = prompt ("right your name")
+alert ("welcome "+ yourName+"")
+// the end of Q.3
+
+
+document.write("<h2>the start of Q.5</h2> <br>")
+var num = prompt("write a number for table ")
+document.write(""+num+" * 1 = "+num * 1 +" <br>")
+document.write(""+num+" * 2 = "+num * 2 +" <br>")
+document.write(""+num+" * 3 = "+num * 3 +" <br>")
+document.write(""+num+" * 4 = "+num * 4 +" <br>")
+document.write(""+num+" * 5 = "+num * 5 +" <br>")
+document.write(""+num+" * 6 = "+num * 6 +" <br>")
+document.write(""+num+" * 7 = "+num * 7 +" <br>")
+document.write(""+num+" * 8 = "+num * 8 +" <br>")
+document.write(""+num+" * 9 = "+num * 9 +" <br>")
+document.write(""+num+" * 10 = "+num * 10 +"")
+document.write("<h2>the end of Q.5</h2> <br>")
+
+
+document.write("<h2>the start of Q.6</h2> <br>")
+var first = prompt("write your first subject")
+var second = prompt("write your second subject")
+var third = prompt("write your third subject")
+var firstObtMark  =prompt("write first period obt marks")
+var secondObtMark  =prompt("write second period obt marks")
+var thirdObtMark  =prompt("write third period obt marks")
+document.write("<h2>"+ first+"      100    "+firstObtMark +"  <br></H2>")
+document.write(" <H2>"+ second+"      100    "+secondObtMark +"  <br> </h2>")
+document.write("<h2>"+ third+"      100    "+thirdObtMark +"  <br></h2> <br> ")
+document.write("<h2>the end of Q.6</h2> <br>")
+
